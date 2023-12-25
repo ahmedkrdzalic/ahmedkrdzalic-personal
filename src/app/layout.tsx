d4401b0e61731b4fbf4f1d282ec782fe9ef37573
ak-zen <ahmed.krdzalic@zendev.se>
2023-12-25 10:33:04 +0100

    GA4 init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import type { Metadata } from "next";
 import "./globals.css";
 import Paws from "@/components/Paws";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Ahmed Krdzalic",
@@ -18,6 +19,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="min-h-screen">
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-EYXMFNXEJ5"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-EYXMFNXEJ5');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
       <body
         className="relative"
         style={{
